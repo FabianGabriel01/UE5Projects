98197f05fc5c384a8387a176a0687dc1a1f4174d
FabianGabriel01 <70608281+FabianGabriel01@users.noreply.github.com>
2022-12-31 13:37:10 -0600

    NewUE5

---
+++ Source/UE5Projects/UE5ProjectsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5ProjectsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5PROJECTS_API AUE5ProjectsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
